2679aafd2339caf5d7d84c835d85d94fd9660bd2
Surya Singh <115163053+suryasingh30@users.noreply.github.com>
2025-07-20 08:57:00 +0530

    Update Blogs.tsx

--- StudentPlus/frontend/src/pages/Blogs.tsx
+++ StudentPlus/frontend/src/pages/Blogs.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { Appbar } from '../components/Appbar';
 import { BlogCard } from '../components/BlogCard';
 import BlogSkeleton from '../components/BlogSkeleton';
